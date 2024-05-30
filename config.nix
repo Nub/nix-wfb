99a383f7d7bdb7bd6036d2c2ae712534d17861d7
Zachry Thayer <zachthayer@gmail.com>
2024-05-30 19:10:11 +0000

    Updated systems

--- config.nix
+++ config.nix
@@ -1,38 +1,31 @@
-{ pkgs, ... }:
-let 
-  nixos_cfg = ./.;
-in{
-  imports = map (x: import x) [ ./wfb.nix ./hardware.nix ];
-
-  networking.hostName = "wfb-pi";
+{ pkgs, inputs, system, ... }:
+{
+  networking.hostName = "wfb";
   networking.wireless.enable = true;
   networking.wireless.userControlled.enable = true;
+
+  # TODO: remove temp wifi networks
   networking.wireless.networks.BigHertz.psk = "somethingbig";
   networking.wireless.networks.LilHertz.psk = "somethinglil";
 
+  # TODO: make these into module options
   networking.useDHCP = false;
   networking.interfaces.wlan0.useDHCP = true;
 
-  users.users.wfb-pi = {
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIngpCTE3a8QDHarFnqa9O08MbmOlPNzptmfQ233yGzn zachthayer@F915Q2RDFY"
-    ];
+  users.users.wfb = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
-    password = "wfb-pi";
+    password = "wfb";
     shell = pkgs.fish;
   };
 
-  environment.variables = {
-    EDITOR = "nvim";
-  };
+  environment.variables = { EDITOR = "nvim"; };
 
   environment.shellAliases = {
     jfu = "journalctl --output cat -fu";
     ctl = "sudo systemctl";
     wfb_drone = "cd ~/wfb; wfb-server udp_drone wfb0";
     wfb_gs = "cd ~/wfb; wfb-server udp_gs wfb0";
-    install_nixos_cfg = "cp -r ${nixos_cfg} ~/nixos_cfg";
     rbs = "sudo nixos-rebuild switch --flake";
   };
 
@@ -65,10 +58,8 @@ in{
   nix.settings.experimental-features = [ "nix-command" "flakes" "repl-flake" ];
   nix.settings.trusted-users = [ "merops" "@wheel" ];
   nix.settings = {
-    substituters = [ 
-      "https://raspberry-pi-nix.cachix.org"
-      "https://wfb-pi.cachix.org"
-    ];
+    substituters =
+      [ "https://raspberry-pi-nix.cachix.org" "https://wfb-pi.cachix.org" ];
     trusted-public-keys = [
       "raspberry-pi-nix.cachix.org-1:WmV2rdSangxW0rZjY/tBvBDSaNFQ3DyEQsVw8EvHn9o="
       "wfb-pi.cachix.org-1:T41vAtTN5vC3hG5wZzVx2+x5Fdk1gjsPAxsJ0uTZK1M="
--- flake.nix
+++ flake.nix
@@ -1,29 +1,45 @@
 {
   description = "nix-wfb nixos configurations for wfb-ng system";
   nixConfig = {
-    extra-substituters = [ 
-      "https://raspberry-pi-nix.cachix.org"
-      "https://wfb-pi.cachix.org"
-    ];
+    extra-substituters =
+      [ "https://raspberry-pi-nix.cachix.org" "https://wfb-pi.cachix.org" ];
     extra-trusted-public-keys = [
       "raspberry-pi-nix.cachix.org-1:WmV2rdSangxW0rZjY/tBvBDSaNFQ3DyEQsVw8EvHn9o="
       "wfb-pi.cachix.org-1:T41vAtTN5vC3hG5wZzVx2+x5Fdk1gjsPAxsJ0uTZK1M="
     ];
   };
   inputs = {
+    utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     raspberry-pi-nix.url = "github:tstat/raspberry-pi-nix";
+    wfb-ng = {
+      url = "github:Nub/wfb-ng/zbt/nix";
+      # ref = "zbt/nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, raspberry-pi-nix }:
-    let
-      inherit (nixpkgs.lib) nixosSystem;
-    in {
-      nixosConfigurations = {
-        wfb-pi = nixosSystem {
-          system = "aarch64-linux";
+  outputs = { self, nixpkgs, utils, ... }@inputs:
+    let inherit (nixpkgs.lib) nixosSystem;
+    in 
+    utils.lib.eachDefaultSystem (system: {
+        wfb = nixosSystem {
+          inherit system;
           modules = [ 
-            raspberry-pi-nix.nixosModules.raspberry-pi
+              inputs.wfb-ng.nixosModules.${system}.wfb
+              (import ./config.nix)
+          ];
+        };
+    }) 
+    // # Inject custom pi variant
+    {
+      nixosConfigurations = {
+        wfb-pi = nixosSystem rec {
+          system = "aarch64-linux";
+          modules = [
+            inputs.raspberry-pi-nix.nixosModules.raspberry-pi
+            (import ./pi-hardware.nix)
+            inputs.wfb-ng.nixosModules.${system}.wfb
             (import ./config.nix)
           ];
         };
